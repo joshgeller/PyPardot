4b91ba12e022c504e64c802d7162a7faffc4e5ab
Josh <overcome@gmail.com>
2014-03-09 10:30:36 -0400

    Refactor prospect multi-use functions

--- pydot/objects/prospects.py
+++ pydot/objects/prospects.py
@@ -16,32 +16,32 @@ class Prospects():
         result = self._get(path='/do/query', params=kwargs)
         return result
 
-    def assign(self, **kwargs):
+    def assign_by_email(self, email=None, **kwargs):
         """
-        Assigns or reassigns the prospect specified by <email> or <id> to a specified Pardot user or
+        Assigns or reassigns the prospect specified by <email> to a specified Pardot user or
         group. One (and only one) of the following parameters must be provided to identify the target user or
         group: <user_email>, <user_id>, or <group_id>. Returns an updated version of the prospect.
         """
-        if 'email' in kwargs:
-            path = '/do/assign/email/{email}'.format(email=kwargs.get('email'))
-        elif 'id' in kwargs:
-            path = '/do/assign/id/{id}'.format(id=kwargs['id'])
-        else:
-            path = '/do/assign/'
-        result = self._post(path=path, params=kwargs)
+        result = self._post(path='/do/assign/email/{email}'.format(email=kwargs.get('email')), params=kwargs)
         return result
 
-    def unassign(self, **kwargs):
+    def assign_by_id(self, id=None, **kwargs):
         """
-        Unassigns the prospect specified by <email> or <id>. Returns an updated version of the prospect.
+        Assigns or reassigns the prospect specified by <id> to a specified Pardot user or
+        group. One (and only one) of the following parameters must be provided to identify the target user or
+        group: <user_email>, <user_id>, or <group_id>. Returns an updated version of the prospect.
         """
-        if 'email' in kwargs:
-            path = '/do/unassign/email/{email}'.format(email=kwargs.get('email'))
-        elif 'id' in kwargs:
-            path = '/do/unassign/id/{id}'.format(id=kwargs.get('id'))
-        else:
-            path = '/do/unassign/'
-        result = self._post(path=path, params=kwargs)
+        result = self._post(path='/do/assign/id/{id}'.format(id=kwargs.get('id')), params=kwargs)
+        return result
+
+    def unassign_by_email(self, email=None, **kwargs):
+        """Unassigns the prospect specified by <email>. Returns an updated version of the prospect."""
+        result = self._post(path='/do/unassign/email/{email}'.format(email=kwargs.get('email')), params=kwargs)
+        return result
+
+    def unassign_by_id(self, id=None, **kwargs):
+        """Unassigns the prospect specified by <id>. Returns an updated version of the prospect."""
+        result = self._post(path='/do/unassign/id/{id}'.format(id=kwargs.get('id')), params=kwargs)
         return result
 
     def create(self, **kwargs):
@@ -51,69 +51,73 @@ class Prospects():
         result = self._post(path='/do/create/email/{email}'.format(email=kwargs.get('email')), params=kwargs)
         return result
 
-    def read(self, **kwargs):
+    def read_by_email(self, email=None, **kwargs):
         """
-        Returns data for the prospect specified by <email> or <id>, including campaign assignment, profile criteria
+        Returns data for the prospect specified by <email>, including campaign assignment, profile criteria
         matching statuses, associated visitor activities, email list subscriptions, and custom field data.
-        <email> is the email address of the target prospect. <id> is the Pardot ID of the target prospect.
+        <email> is the email address of the target prospect.
         """
-        if 'email' in kwargs:
-            path = '/do/read/email/{email}'.format(email=kwargs.get('email'))
-        elif 'id' in kwargs:
-            path = '/do/read/id/{id}'.format(id=kwargs.get('id'))
-        else:
-            path = '/do/read/'
-        result = self._post(path=path, params=kwargs)
+        result = self._post(path='/do/read/email/{email}'.format(email=kwargs.get('email')), params=kwargs)
         return result
 
-    def update(self, **kwargs):
+    def read_by_id(self, id=None, **kwargs):
         """
-        Updates the provided data for a prospect specified by <email> or <id>. <email> is the email address of the
-        prospect. <id> is the Pardot ID of the prospect. Fields that are not updated by the request remain unchanged.
+        Returns data for the prospect specified by <id>, including campaign assignment, profile criteria
+        matching statuses, associated visitor activities, email list subscriptions, and custom field data.
+        <id> is the Pardot ID of the target prospect.
+        """
+        result = self._post(path='/do/read/id/{id}'.format(id=kwargs.get('id')), params=kwargs)
+        return result
+
+    def update_by_email(self, email=None, **kwargs):
+        """
+        Updates the provided data for a prospect specified by <email>. <email> is the email address of the
+        prospect. Fields that are not updated by the request remain unchanged.nEmail list subscriptions and custom
+        field data may also be updated with this request.
+        """
+        result = self._post(path='/do/update/email/{email}'.format(email=kwargs.get('email')), params=kwargs)
+        return result
+
+    def update_by_id(self, id=None, **kwargs):
+        """
+        Updates the provided data for a prospect specified by <id>. <id> is the Pardot ID of the prospect.
+        Fields that are not updated by the request remain unchanged. Email list subscriptions and custom field data
+        may also be updated with this request.
+        """
+        result = self._post(path='/do/update/id/{id}'.format(id=kwargs.get('id')), params=kwargs)
+        return result
+
+    def upsert_by_email(self, email=None, **kwargs):
+        """
+        Updates the provided data for the prospect specified by <email>. If a prospect with the provided email address
+        does not yet exist, a new prospect is created using the <email> value. Fields that are not updated by the
+        request remain unchanged. Email list subscriptions and custom field data may also be updated with this request.
+        """
+        result = self._post(path='/do/upsert/email/{email}'.format(email=kwargs.get('email')), params=kwargs)
+        return result
+
+    def upsert_by_id(self, id=None, email=None, **kwargs):
+        """
+        Updates the provided data for the prospect specified by <id>. If an <email> value is provided, it is used to
+        update the prospect's email address. If a prospect with the provided ID is not found, Pardot searches for a
+        prospect identified by <email>. If a prospect with the provided email address does not yet exist, a new
+        prospect is created using <email> value. Fields that are not updated by the request remain unchanged.
         Email list subscriptions and custom field data may also be updated with this request.
         """
-        if 'email' in kwargs:
-            path = '/do/update/email/{email}'.format(email=kwargs.get('email'))
-        elif 'id' in kwargs:
-            path = '/do/update/id/{id}'.format(id=kwargs.get('id'))
-        else:
-            path = '/do/update/'
-        result = self._post(path=path, params=kwargs)
+        result = self._post(path='/do/upsert/id/{id}'.format(id=kwargs.get('id')), params=kwargs)
         return result
 
-    def upsert(self, **kwargs):
-        """
-        Updates the provided data for a prospect specified by <email> or <id>. If a prospect with the provided email
-        address does not yet exist, a new prospect is created using the <email> value. <email> is the email address of
-        the prospect. <id> is the Pardot ID of the prospect. Fields that are not updated by the request remain
-        unchanged. Email list subscriptions and custom field data may also be updated with this request.
-        """
-        if 'email' in kwargs:
-            path = '/do/upsert/email/{email}'.format(email=kwargs.get('email'))
-        elif 'id' in kwargs:
-            path = '/do/upsert/id/{id}'.format(id=kwargs.get('id'))
-        else:
-            path = '/do/upsert/'
-        result = self._post(path=path, params=kwargs)
+    def delete_by_email(self, email=None, **kwargs):
+        # TODO check on HTTP response
+        """Deletes the prospect specified by <email>"""
+        result = self._post(path='/do/delete/email/{email}'.format(email=kwargs.get('email')), params=kwargs)
         return result
 
-    def delete(self, **kwargs):
-        """
-        Deletes the prospect specified by <email>.
-        """
-        if 'email' in kwargs:
-            path = '/do/delete/email/{email}'.format(email=kwargs.get('email'))
-        elif 'id' in kwargs:
-            path = '/do/delete/id/{id}'.format(id=kwargs.get('id'))
-        else:
-            path = '/do/delete/'
-        try:
-            self._post(path=path, params=kwargs)
-        # API doesn't return JSON, so we'll get a ValueError on success.
-        # TODO Find a way to clean this up.
-        except ValueError:
-            pass
-
+    def delete_by_id(self, id=None, **kwargs):
+        # TODO check on HTTP response
+        """Deletes the prospect specified by <id>"""
+        result = self._post(path='/do/delete/id/{id}'.format(id=kwargs.get('id')), params=kwargs)
+        return result
 
     def _get(self, path=None, params={}):
         """GET requests for the Prospect object"""
