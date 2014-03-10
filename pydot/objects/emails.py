60274245af0f22c7c9c84227adcffda628df2ba3
Josh <overcome@gmail.com>
2014-03-10 08:07:24 -0400

    Add required parameters to function comments

--- pydot/objects/emails.py
+++ pydot/objects/emails.py
@@ -8,7 +8,10 @@ class Emails():
         self.client = client
 
     def send_to_email(self, prospect_email=None, **kwargs):
-        """Sends an email to the prospect identified by <prospect_email>"""
+        """
+        Sends an email to the prospect identified by <prospect_email>
+        Required parameters: (email_template_id OR (text_content, name, subject, & ((from_email & from_name) OR from_user_id)))
+        """
         kwargs['prospect_email'] = prospect_email
         result = self._post(
             path='/do/send/prospect_email/{prospect_email}'.format(prospect_email=kwargs.get('prospect_email')),
@@ -16,14 +19,20 @@ class Emails():
         return result
 
     def send_to_id(self, prospect_id=None, **kwargs):
-        """Sends an email to the prospect identified by <prospect_id>"""
+        """
+        Sends an email to the prospect identified by <prospect_id>
+        Required parameters: (email_template_id OR (text_content, name, subject, & ((from_email & from_name) OR from_user_id)))
+        """
         kwargs['prospect_id'] = prospect_id
         result = self._post(
             path='/do/send/prospect_id/{prospect_id}'.format(prospect_id=kwargs.get('prospect_id')), params=kwargs)
         return result
 
     def send_to_lists(self, list_ids=None, **kwargs):
-        """Sends an email to the lists identified by <list_ids>"""
+        """
+        Sends an email to the lists identified by <list_ids>
+        Required parameters: (email_template_id OR (text_content, name, subject, & ((from_email & from_name) OR from_user_id)))
+        """
         kwargs['list_ids'] = list_ids
         result = self._post(
             path='/do/send/', params=kwargs)
