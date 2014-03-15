fab80f9b0b348e728a2925f907729ddb94691e49
Josh <overcome@gmail.com>
2014-03-15 15:31:16 -0400

    Add distutils info

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pypardot',
+    packages=['pypardot'],  # this must be the same as the name above
+    version='0.1',
+    description='An API wrapper for Pardot marketing automation software.',
+    author='Josh Geller',
+    author_email='joshualgeller@gmail.com',
+    url='https://github.com/joshgeller/PyPardot',  # use the URL to the github repo
+    download_url='https://github.com/joshgeller/PyPardot/tarball/0.1',  # I'll explain this in a second
+    keywords=['pardot', 'api', 'wrapper'],  # arbitrary keywords
+    classifiers=[],
+)
