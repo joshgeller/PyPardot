3b484b8285befc186f02f334d8ce9a2e24093b02
Josh Geller <joshualgeller@gmail.com>
2014-03-20 12:33:39 -0400

    Adds required campaign ID parameter

--- pypardot/objects/emails.py
+++ pypardot/objects/emails.py
@@ -7,33 +7,33 @@ class Emails():
     def __init__(self, client):
         self.client = client
 
-    def send_to_email(self, prospect_email=None, **kwargs):
+    def send_to_email(self, campaign_id=None, prospect_email=None, **kwargs):
         """
         Sends an email to the prospect identified by <prospect_email>.
         Required parameters: (email_template_id OR (text_content, name, subject, & ((from_email & from_name) OR from_user_id)))
         """
-        kwargs['prospect_email'] = prospect_email
+        kwargs.update({'prospect_email': prospect_email, 'campaign_id': campaign_id})
         result = self._post(
             path='/do/send/prospect_email/{prospect_email}'.format(prospect_email=kwargs.get('prospect_email')),
             params=kwargs)
         return result
 
-    def send_to_id(self, prospect_id=None, **kwargs):
+    def send_to_id(self, campaign_id=None, prospect_id=None, **kwargs):
         """
         Sends an email to the prospect identified by <prospect_id>.
         Required parameters: (email_template_id OR (text_content, name, subject, & ((from_email & from_name) OR from_user_id)))
         """
-        kwargs['prospect_id'] = prospect_id
+        kwargs.update({'prospect_id': prospect_id, 'campaign_id': campaign_id})
         result = self._post(
             path='/do/send/prospect_id/{prospect_id}'.format(prospect_id=kwargs.get('prospect_id')), params=kwargs)
         return result
 
-    def send_to_lists(self, list_ids=None, **kwargs):
+    def send_to_lists(self, campaign_id=None, list_ids=None, **kwargs):
         """
         Sends an email to the lists identified by <list_ids>.
         Required parameters: (email_template_id OR (text_content, name, subject, & ((from_email & from_name) OR from_user_id)))
         """
-        kwargs['list_ids'] = list_ids
+        kwargs.update({'list_ids': list_ids, 'campaign_id': campaign_id})
         result = self._post(
             path='/do/send/', params=kwargs)
         return result
